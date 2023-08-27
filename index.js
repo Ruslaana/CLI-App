5de5561aa585f433c86381402dc8a4f9dae08de2
Ruslana Onyshchuk <lalelya7@gmail.com>
2023-08-27 14:28:50 +0200

    add screenshots with the result of each command

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// const contacts = require('./contacts');
-
-// async function main() {
-//   await contacts.addContact(
-//     'Іван Іванов', 
-//     'ivan@example.com', 
-//     '123-456-7890');
-//   await contacts.addContact(
-//     'Петро Петров',
-//     'petro@example.com',
-//     '987-654-3210',
-//   );
-
-//   const allContacts = await contacts.listContacts();
-//   console.log('Список всіх контактів:', allContacts);
-
-//   const contactId = allContacts[1].id;
-//   const contactById = await contacts.getContactById(contactId);
-//   console.log('Контакт за ID:', contactById);
-
-//   const removedContact = await contacts.removeContact(contactId);
-//   console.log('Видалений контакт:', removedContact);
-
-//   const updatedContacts = await contacts.listContacts();
-//   console.log('Список після видалення:', updatedContacts);
-// }
-
-// main();
-
 const { Command } = require('commander');
 const program = new Command();
 const { listContacts, getContactById, addContact, removeContact } = require('./contacts');
